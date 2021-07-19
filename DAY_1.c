f57aad06890757e877559bb90537c8756a9d12b2
Phw9 <hyunwoo1417@hanyang.ac.kr>
2021-07-19 16:09:18 +0900

    Day 1

--- DAY_1.c
+++ DAY_1.c
@@ -2,7 +2,43 @@
 
 int main(void)
 {
-	printf("hello world\n");
+	
+// 2-1
+	/*printf("홍길동\n홍 길 동\n홍  길  동\n");
+
+	printf("PHW\n");
+	printf("서울시~~~\n");
+	printf("01049090164\n");*/
+	
+	
+//2-2
+
+	/*printf("제 이름은 홍길동입니다.\n");
+	printf("제 나이는 %d살이고요.\n", 20);
+	printf("제가 사는 곳의 번지수는 %d-%d입니다.\n", 123, 456);*/
+
+
+//VarDeclAndInit.c
+
+	/*int num1, num2, num3, num4;
+
+	printf("num1: %d, num2: %d\n", num1, num2); //쓰레기값 출력
+
+	num1 = 10, num2 = 20, num3 = 30, num4 = 40;
+
+	printf("num1: %d, num2: %d\n", num1, num2);
+	printf("num3: %d, num4: %d\n", num3, num4);*/
+
+	
+//SimpleAddTwo.c
+	
+	/*int num1 = 3, num2 = 4;
+	int result = num1 + num2;
+
+	printf("덧셈 결과: %d\n", result);
+	printf("%d+%d=%d\n", num1, num2, result);
+	printf("%d+%d=%d\n", num1, num2, num1 + num2);*/
+	
 
 	return 0;
 
