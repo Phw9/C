4763fbaae986c2c4f34e5c08427c56acf52c0f18
Phw9 <hyunwoo1417@hanyang.ac.kr>
2021-07-19 17:38:18 +0900

    Day 2

---
+++ DAY_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void)
+{
+	//OperatorOne.c
+
+	/*int num1 = 9, num2 = 2;
+	printf("%d£«%d = %d\n", num1, num2, num1 + num2);
+	printf("%d£­%d = %d\n", num1, num2, num1 - num2);
+	printf("%d¡¿%d = %d\n", num1, num2, num1 * num2);
+	printf("%d¡À%dÀÇ ¸ò= %d\n", num1, num2, num1 / num2);
+	printf("%d¡À%dÀÇ ³ª¸ÓÁö= %d\n", num1, num2, num1 % num2);*/
+
+	
+	//¸ğµÎ °ÅÁşÀÌ¿©µµ °ÅÁş
+
+	/*int num1 = 2, num2 = 4, num3 = 6;
+
+	printf("return: %d\n", num1 > 1 && num2 > 2);
+	printf("return: %d\n", num1 < 1 && num2 < 2); */
+
+
+	return 0;
+
+}
