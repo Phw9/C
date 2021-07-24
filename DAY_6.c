2fba62c3fb08eb8b405d56dfc2822345a1a2a419
Phw9 <hyunwoo1417@hanyang.ac.kr>
2021-07-24 23:59:28 +0900

    Day 6,7

---
+++ DAY_6.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	//ArrayNamePointerOperation.c
+
+	/*int arr1[3] = { 1,2,3 };
+	double arr2[3] = { 1.1,2.2,3.3 };
+
+	printf("%d %g \n", *arr1, *arr2);
+	*arr1 += 100;
+	*arr2 += 120.5;
+	printf("%d %g\n", arr1[0], arr2[0]);*/
+
+	//ArrayNameIsPointer.c
+
+	//int arr[3] = { 1,2,3 };
+	//int* ptr = &arr[0]; // int* ptr = &arr; 과 같은 문장임
+
+	//printf("%d %d \n", ptr[0], arr[0]);
+	//printf("%d %d \n", ptr[1], arr[1]);
+	//printf("%d %d \n", ptr[2], arr[2]);
+	//printf("%d %d \n \n", *ptr, *arr);
+
+	//*ptr += 100;
+	//printf("%d %d \n", ptr[1], arr[1]);
+	//printf("%d %d \n \n", *ptr, *arr);
+	
+
+
+
+
+	return 0;
+}
